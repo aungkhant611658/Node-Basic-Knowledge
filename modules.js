dc943b45832525fe03974987ed40f15f12309426
aungkhant611658 <aungkhant611658@gmail.com>
2023-07-29 09:40:16 +0630

    first commit

---
+++ modules.js
@@ -0,0 +1,8 @@
+const names = require("./names.js");
+const sayHi = require("./utils.js");
+const data = require("./altarnative-flavour.js");
+require("./mind-grenade.js");
+
+sayHi(names.john);
+
+console.log(data);
---
+++ names.js
@@ -0,0 +1,10 @@
+// Local
+const secret = "SUPER SECRET";
+
+// Share
+const john = "john";
+const peter = "peter";
+
+module.exports = { john, peter };
+
+// console.log(module);
