1987f8e9d935998daab15597f206c5ca1c138948
aungkhant611658 <aungkhant611658@gmail.com>
2023-08-07 20:27:09 +0630

    add some method

---
+++ 2 - modules.js
@@ -0,0 +1,8 @@
+const names = require("./3 - names");
+const sayHi = require("./4- utils.js");
+const data = require("./5 - altarnative-flavour.js");
+require("./6 - mind-grenade.js");
+
+sayHi(names.john);
+
+console.log(data);
--- modules.js
+++
@@ -1,8 +0,0 @@
-const names = require("./names.js");
-const sayHi = require("./utils.js");
-const data = require("./altarnative-flavour.js");
-require("./mind-grenade.js");
-
-sayHi(names.john);
-
-console.log(data);
